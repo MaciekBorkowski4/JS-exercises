718cae24c58f6f44b0ccd10528c1062d82e077c3
MBorkowski <mb.maciejborkowski@gmail.com>
2017-01-03 23:19:02 +0100

    css variables exercises finished

--- CSS variables/script.js
+++ CSS variables/script.js
@@ -1,1 +1,8 @@
+const inputs = document.querySelectorAll('input');
 
+function handleChange () {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+inputs.forEach(input => input.addEventListener('change', handleChange));
+inputs.forEach(input => input.addEventListener('mousemove', handleChange));
