6abfa7a21e153fb9f6d17548e5144e2ee139c515
MBorkowski <mb.maciejborkowski@gmail.com>
2017-01-03 10:45:52 +0100

    uruchomienie pracy sekund, minut i godzin wraz ze wskazówkami

--- js clock/script.js
+++ js clock/script.js
@@ -1,7 +1,23 @@
+const handSecond = document.querySelector(`.hand-second`);
+const handMinute = document.querySelector(`.hand-minute`);
+const handHour = document.querySelector(`.hand-hour`);
+
 function setDate () {
     const now = new Date();
     const seconds = now.getSeconds();
-    console.log(seconds);
+    const secondDegree = ((seconds / 60) * 360) + 90;
+handSecond.style.transform = `rotate(${secondDegree}deg)`;
+console.log(seconds);
+
+const mins = now.getMinutes();
+const minsDegree = ((mins / 60)*360) + 90;
+handMinute.style.transform = `rotate(${minsDegree}deg)`;
+console.log(mins);
+
+const hours = now.getHours();
+const hoursDegree = ((hours / 12)*360) + 90;
+handHour.style.transform = `rotate(${hoursDegree}deg)`;
+console.log(hours);
 
 }
 
